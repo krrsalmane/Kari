87914a7eab10033a5c90a74c1dd30621cc14edb1
krrsalmane <karroumsalmane@gmail.com>
2026-01-08 12:38:59 +0100

    config: setup email service configuration

---
+++ config/email.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+    'host' => 'smtp.gmail.com',           
+    'username' => 'karroumsalmane@gmail.com', 
+    'password' => 'zqdtqndzmpwjifwj',    
+    'port' => 587,                      
+    'from_email' => 'noreply@kari.com',
+    'from_name' => 'Kari Rentals'
+];
